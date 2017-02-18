5e8908525510fd17f5302cde9c66266c191e6e40
Kyle Thomson <kjay.thomson@gmail.com>
2017-02-18 13:05:32 +0100

    rm statics

--- core/static/js/ReactComponent.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ReactTest extends Component {
-    render() {
-		const { rendered } = this.props
-		return <span>This is rendered by the {rendered}</span> 
-    }
-}
